cfb259334e0ce2c2864c8f970f579ea9ab87426b
Dani Lyn <dlyn1110@gmail.com>
2018-07-04 21:08:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,7 @@ class Student
     sql = <<-SQL
       SELECT COUNT(*)
       FROM students
-      WHERE grade < 11
+      WHERE students.grade < 12
     SQL
 
     DB[:conn].execute(sql).map do |row|
