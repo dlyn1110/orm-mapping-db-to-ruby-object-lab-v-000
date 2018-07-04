9021f183ee0d17e23f2c373b6137f03b75a7b01c
Dani Lyn <dlyn1110@gmail.com>
2018-07-04 20:54:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,12 @@ class Student
    end
 
    def self.first_student_in_grade_10
-     
+     sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
    end
 
    def self.first_X_students_in_grade_10(num)
