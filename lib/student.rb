3b251ccdbae563052a54d21ac342643fab9a660c
Dani Lyn <dlyn1110@gmail.com>
2018-07-04 20:10:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
 
   def self.new_from_db(row)
     student = self.new# self.new is the same as running Song.new
-    new_song.id = row[0]
+    new_.id = row[0]
     new_song.name = row[1]
     new_song.length = row[2]
     new_song # return the newly created instance
