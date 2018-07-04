1044bd346f37d10aee2221e3d100fcab441ed89f
Dani Lyn <dlyn1110@gmail.com>
2018-07-04 20:38:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,35 +2,40 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    new_student = self.new     # self.new is the same student.new
+    # create a new Student object given a row from the database
+    new_student = self.new
     new_student.id = row[0]
-    new_student.name = row[1]
+    new_student.name =  row[1]
     new_student.grade = row[2]
-    new_student                # return the newly created instance
-                              # create a new Student object given a row from the database
+    new_student
   end
 
-  def self.all                    # retrieve all the rows from the "Students" database
+  def self.all
+    # retrieve all the rows from the "Students" database
+    # remember each row should be a new instance of the Student class
     sql = <<-SQL
-    SELECT *
-    FROM students
+      SELECT *
+      FROM students
     SQL
     DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)# remember each row should be a new instance of the Student class
+      self.new_from_db(row)
     end
-   end
+  end
 
   def self.find_by_name(name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
     sql = <<-SQL
-    SELECT FROM students
-    WHERE name = ?
-    LIMIT 1
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
     SQL
-   # find the student in the database given a name
     DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)# return a new instance of the Student class
-    end.first
+     self.new_from_db(row)
+   end.first
   end
+
 
   def self.count_all_students_in_grade_9
     sql = <<-SQL
